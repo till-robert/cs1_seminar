40a02af240efa1520ce1704e19efe2489d0576d7
Till Pfaff <till.pfaff@tu-dortmund.de>
2023-01-20 21:55:28 +0100

    update tasks

--- task20/task20.cpp
+++ task20/task20.cpp
@@ -22,7 +22,35 @@ const int seed = 42;
 std::mt19937 gen(seed); //Standard mersenne_twister_engine seeded with rd()
 std::uniform_real_distribution<> real_distrib(0.0,1.0);
 std::uniform_int_distribution<> bool_distrib(0,1);
+std::uniform_int_distribution<> neighbor_distrib(0,3);
+struct RandomNeighbor //used for 2D positions
+{
+    int x, y;
 
+    RandomNeighbor() = default;
+    RandomNeighbor(int p_x, int p_y) { //direction can be 0: LEFT, 1: UP, 2: RIGHT, 3: DOWN
+            int dir = neighbor_distrib(gen);
+            switch (dir)
+            {
+            case 0:
+                x = p_x-1;
+                y = p_y;
+                break;
+            case 1:
+                x = p_x;
+                y = p_y+1;
+                break;
+            case 2:
+                x = p_x+1;
+                y = p_y;
+                break;
+            case 3:
+                x = p_x;
+                y = p_y-1;
+                break;
+            }
+    }
+};
 
 struct Spin{ //underlying structure is a boolean, but casting/setting with -1,1,
     bool spin_bool;
@@ -35,8 +63,9 @@ struct Spin{ //underlying structure is a boolean, but casting/setting with -1,1,
     //     spin_bool = s+1;
     //     return s;
     // }
-    inline bool flip(){
-        return spin_bool = !spin_bool;
+    inline Spin flip(){
+        spin_bool = !spin_bool;
+        return *this;
     }
 };
 class SpinSystem{
@@ -73,16 +102,29 @@ public:
     }
     inline signed char spinAt(int i, int j){
         if(periodic){
-            if(i == -1) i = L-1;
-            else if(i == L) i = 0;
+            if(i <= -1) i += L;
+            else if(i >= L) i -= L;
 
-            if(j < 0) j = L-1;
-            else if(j >= L) j = 0;
+            if(j <= -1) j += L;
+            else if(j >= L) j -= L;
 
             return spins[i][j];
         }
         //free boundary conditions: return 0 for spins out of bounds
         return (i < L && i >= 0 && j < L && j >= 0) ? spins[i][j] : 0;
+    }
+    inline signed char safe_flip(int i, int j){
+        if(periodic){
+            if(i <= -1) i += L;
+            else if(i >= L) i -= L;
+
+            if(j <= -1) j += L;
+            else if(j >= L) j -= L;
+
+            return spins[i][j].flip();
+        }
+        //free boundary conditions: return 0 for spins out of bounds
+        return (i < L && i >= 0 && j < L && j >= 0) ? spins[i][j].flip() : 0;
     }
     // void setBeta(double beta){
     //     flip_probabilities = {exp(-beta*2),exp(-beta*4)};
@@ -114,21 +156,17 @@ public:
             }
             return spinAt(i,j);
         }
-        else{//kawasaki => M=const, dM=0
-            int random_i, random_j;
-            do{ //find suitable spin pair
-                random_i = random_spin_choice_distrib(gen);
-                random_j = random_spin_choice_distrib(gen);
-            }while(spinAt(random_i,random_j) == spinAt(i,j));
-            //dM = 0;
+        else{//kawasaki => M=const
+            RandomNeighbor rn(i,j);
             signed char dE =   2 * spinAt(i,j) * (spinAt(i-1,j) + spinAt(i+1,j)+ spinAt(i,j-1) + spinAt(i,j+1)); //if i is out of bounds, spinAt(i) returns 0
-                             + 2 * spinAt(random_i,random_j) * (spinAt(random_i-1,random_j) + spinAt(random_i+1,random_j)+ spinAt(random_i,random_j-1) + spinAt(random_i,random_j+1)); //if i is out of bounds, spinAt(i) returns 0
+                             + 2 * spinAt(rn.x,rn.y) * (spinAt(rn.x-1,rn.y) + spinAt(rn.x+1,rn.y)+ spinAt(rn.x,rn.y-1) + spinAt(rn.x,rn.y+1)); //if i is out of bounds, spinAt(i) returns 0
+            signed char dM = - 2 * (spinAt(i,j) + spinAt(rn.x,rn.y));
 
             unsigned char dE_index = dE/2 + 8;
             double rand = real_distrib(gen);
-            if(rand < flip_probabilities[dE_index]){ //only flip with probability exp(-beta*dE)
+            if(rand < flip_probabilities[dE_index] && dM == 0){ //only flip with probability exp(-beta*dE) and the spins are opposite
                 spins[i][j].flip();
-                spins[random_i][random_j].flip();
+                safe_flip(rn.x,rn.y); //this method checks for out of bounds first
                 acceptance_per_sweep ++;
                 E_J += dE;
             }
@@ -199,7 +237,7 @@ int main(int argc, char* argv[]){
     std::ofstream out("out/avg_timeseries.txt");
     std::ofstream snapshots("out/snapshots.txt");
     
-    int N_systems = 50;
+    int N_systems = 5;
     std::vector<SpinSystem> systems;
     systems.reserve(N_systems);
     for(int j = 0; j < N_systems; j++){
@@ -212,17 +250,17 @@ int main(int argc, char* argv[]){
 
     std::cout << "measuring: 0 %" << std::flush;
     for(int i = 0; i < N_sweeps_measurement; i++){
-        int sum_M = 0;
+        int sum_E = 0;
         int sum_acceptance = 0;
         if(i == 9 || i == 99 || i == 999 || i == 9999){
             snapshots << (std::string) systems[0] << "\n";
         }
         for(int j = 0; j < N_systems; j++){
-            sum_M += abs(systems[j].M);
+            sum_E += systems[j].E_J;
             sum_acceptance += systems[j].acceptance_per_sweep;
             systems[j].sweep();
         }
-        out << (double) sum_M / N_systems << " " <<  (double) sum_acceptance / N_systems << "\n"; //print average to file
+        out << (double) sum_E / N_systems << " " <<  (double) sum_acceptance / N_systems << "\n"; //print average to file
 
         std::cout << "\rmeasuring... " << i * 100 / N_sweeps_measurement << " %" << std::flush;
     }
